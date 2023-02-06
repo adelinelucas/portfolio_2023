25daa04cae3a5fc8296679a818acce95749df34e
adelinelucas <82570582+adelinelucas@users.noreply.github.com>
2023-02-06 21:36:06 +0100

    add router and add style

---
+++ src/components/Layout/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Navbar';
+import './style.css';
+const Layout = () => {
+    return (
+        <section className='layout-section'>
+          <Navbar/>  
+          Layout
+        </section>
+    );
+};
+
+export default Layout;
