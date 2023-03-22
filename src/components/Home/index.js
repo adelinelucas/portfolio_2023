1733b0909e90819a5a5056d55fc74e45d7cc7948
adelinelucas <82570582+adelinelucas@users.noreply.github.com>
2023-03-22 18:38:10 +0100

    add animation on baseline title

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 
 const Home = () => {
+    let [baseline, setBaseline] = useState([]);
+    let [baselineIndex, setBaselineIndex]= useState(0)
+    const speed = 150;
+    const injectTab = () =>{
+        let tab = document.getElementById('baseline-tab')
+        tab.style.borderRight = '4px solid #190F26';
+        tab.classList.add('tab-animated')
+    }
+
+
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            let timer ;
+            const baselineComplete= ' Adeline, dévelopeuse junior';
+
+            if(baselineIndex < baselineComplete.length){
+                setBaseline(oldArray => [...oldArray,baselineComplete[baselineIndex]])
+                console.log(baselineIndex, baseline)
+                setBaselineIndex(baselineIndex++);
+                if(baselineIndex === baselineComplete.length){
+                    injectTab()
+                }
+            }else{
+                clearTimeout(timer);
+            }
+        },speed)
+
+        return ()=>{
+            clearInterval(interval)
+            setBaselineIndex(0);
+            setBaseline([])
+        }
+    }, [])
     return (
         <article className='home__article'>
-            <h1>Adeline,<span>développeuse junior en reconversion !</span></h1>
+            <h1 id="baseline">{baseline}<span id="baseline-tab"></span>
+            </h1>
             <p>Après une année d'alternance en tant que dévelopeurse full stack, je souhaite rejoindre une équipe de dev !</p>
             <p>Profil fullstack avec une petite préférence pour le front.</p>
             <p>A travers ce portfolio je vous partage les projets que j'ai pu développer en entreprise ou en formation.</p>
