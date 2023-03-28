1959c887ea510dfecb8f9ed41b3c9d3ea6cb6ad9
adelinelucas <82570582+adelinelucas@users.noreply.github.com>
2023-03-28 16:44:16 +0200

    modify css

--- src/components/DisplayWebProjects/index.js
+++ src/components/DisplayWebProjects/index.js
@@ -6,17 +6,21 @@ const DisplayWebProjects = ({project, idx}) => {
         <article className='portfolio__section__article' key={idx}>
            <img className='portfolio__section__article-cover' src={project.cover?project.cover : './images/internet.png' } alt={project.title}/>
             <div className='portfolio__section__article-content'>
-                <h3 className='portfolio__section__article-title'>{project.titre}</h3>
-                <p className='portfolio__section__article-description'> {project.description}</p>
-                <p>
+                <div className='portfolio__section__article-content-header'>
+                    <h3 className='portfolio__section__article-title'>{project.titre}</h3>
+                    <p>
                     {
                         project.technosTag.map((tech, index)=>(
                             <span key={index}>{tech}</span>
                         ))
                     }
                 </p>
-                <a href={project.url} target="_blank" onClick={()=> window.open(project.url)}>Voir le project</a>
-                <h4>{project.typeProjet}</h4>
+                </div>
+                <p className='portfolio__section__article-description'> {project.description}</p>
+                <div className='portfolio__section__article-content-footer'>
+                    <h4>{project.typeProjet}</h4>
+                    <a href={project.url} target="_blank" onClick={()=> window.open(project.url)}>Voir le project</a>
+                </div >
             </div> 
         </article>
     );
