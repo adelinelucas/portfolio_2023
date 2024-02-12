b3255fdc59e91e696a5ec9df49fcd97b523e0e42
adelinelucas <82570582+adelinelucas@users.noreply.github.com>
2024-02-12 23:54:49 +0100

    add experience pro section

--- src/appv2/pages/SalesforceExperiencesPro/index.js
+++ src/appv2/pages/SalesforceExperiencesPro/index.js
@@ -1,10 +1,88 @@
 import React from 'react';
+import './style.css'; 
+import { Chrono } from "react-chrono";
+import data from '../../../global/datas/exppro.json';
+import { FaComputer } from "react-icons/fa6";
+import { IoBusinessSharp } from "react-icons/io5";
+import { TiBusinessCard } from "react-icons/ti";
+
 
 const SalesforceExperiencesPro = () => {
+    const {webDev, avantReconversion, formationUniversitaire} = data;
     return (
-        <div>
-            experience pro
-        </div>
+        <section className='page__salesforce-profil-exppro' id="page__salesforce-profil-exppro">
+            <h2 className=''>Mes expériences professionnelles</h2>
+            <div className=''>
+                <h3 className=''><span className='title-icon'><FaComputer /></span>Mes expériences web dev</h3>
+                <Chrono
+                    items={webDev.reverse()}
+                    mode="VERTICAL"
+                    timelinePointShape="square"
+                    timelinePointDimension={20}
+                    theme={{
+                        primary: "white",
+                        secondary: "#504EE8",
+                        cardTitleColor: "#504EE8",
+                        cardSubtitleColor:"#8878B2",
+                        titleColorActive:"#F2F0F2",
+                        titleColor:"#F2F0F2",
+                    }}
+                />
+            </div>
+            <div>
+                <h3 className=''><span className='title-icon'><IoBusinessSharp /></span>Mes expériences avant reconversion</h3>
+                <div className='page__salesforce-profil-exppro-horizontal-timeline'>
+                    <Chrono
+                        items={avantReconversion}
+                        mode="HORIZONTAL"
+                        itemWidth={250}
+                        showSingle
+                        timelinePointShape="square"
+                        timelinePointDimension={20}
+                        theme={{
+                            primary: "white",
+                            secondary: "#504EE8",
+                            cardTitleColor: "#504EE8",
+                            cardSubtitleColor:"#8878B2",
+                            titleColorActive:"#F2F0F2",
+                            titleColor:"#F2F0F2",
+                        }}
+                        fontSizes={{
+                            cardSubtitle: '0.85rem',
+                            cardText: '0.8rem',
+                            cardTitle: '1rem',
+                            title: '1rem',
+                        }}
+                    />
+                </div>
+            </div>
+            <div className='page__salesforce-profil-exppro-horizontal-timeline-deux'>
+                <h3 className=''><span className='title-icon'><TiBusinessCard /></span>Ma formation universitaire</h3>
+                <Chrono
+                    items={formationUniversitaire}
+                    mode="HORIZONTAL"
+                    itemWidth={350}
+                    showSingle
+                    timelinePointShape="square"
+                    timelinePointDimension={20}
+                    theme={{
+                        primary: "white",
+                        secondary: "#504EE8",
+                        cardTitleColor: "#504EE8",
+                        cardSubtitleColor:"#8878B2",
+                        titleColorActive:"#F2F0F2",
+                        titleColor:"#F2F0F2",
+                    }}
+                    fontSizes={{
+                        cardSubtitle: '0.85rem',
+                        cardText: '0.8rem',
+                        cardTitle: '1rem',
+                        title: '1rem',
+                    }}
+                    cardHeight={50}
+                 />
+            </div>
+        </section>
     );
 };
 
--- src/appv2/pages/SalesforceHome/index.js
+++ src/appv2/pages/SalesforceHome/index.js
@@ -4,12 +4,14 @@ import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import SalesforceAbout from '../SalesforceAbout';
 import SalesforceContact from '../SalesforceContact';
+import SalesforceExperiencesPro from '../SalesforceExperiencesPro';
 const SalesforceHome = () => {
     return (
         <main className='page__salesforce-profil'>
             <Header/>
             <SalesforceAbout/>
             <div className='page__salesforce-profil-content'>
+                <SalesforceExperiencesPro />
                 <SalesforceContact />
             </div>
             <Footer/>
