96760c533b2143d94693f520383eda7d47a74b70
adelinelucas <82570582+adelinelucas@users.noreply.github.com>
2024-02-27 18:42:26 +0100

    css responsive for web project swiper section

--- src/appv2/components/WebProjects/index.js
+++ src/appv2/components/WebProjects/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import data from '../../../global/datas/webprojects.json';
@@ -19,14 +19,26 @@ const WebProjects = () => {
     const {languageEng} = useLanguageContext();
     let {projetsWeb} = data;
     projetsWeb = [...projetsWeb].reverse().filter((projet) => projet.visibility === "true");
-
+    const [widthSize, setWidthSize] = useState({width: window.innerWidth});
+    const [smallScreenSwiper, setSmallScreenSwiper] = useState(false)
     // if(languageEng) {
     //     projetsWeb = data_en.webProjects;
     //     projetsWeb = [...projetsWeb].reverse().filter((project) => project.visibility === "true");
     // }else{
     //     projetsWeb = [...projetsWeb].reverse().filter((projet) => projet.visibility === "true");
     // }
-    
+    const handleResize = () => {
+        setWidthSize(window.innerWidth);
+    };
+
+    const switchSwiper = () => {
+        if(widthSize < '1400'){
+            setSmallScreenSwiper(true)
+        }else{
+            setSmallScreenSwiper(false)
+        }
+    };
+
     useEffect(()=>{
         if(languageEng) {
             projetsWeb = data_en.webProjects;
@@ -37,7 +49,17 @@ const WebProjects = () => {
         }
     },[languageEng])
 
+    useEffect(()=>{
+        window.addEventListener('resize', handleResize);
+        switchSwiper(widthSize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+          };
+    },[])
+
     return (
+        <>
+        { !smallScreenSwiper ?
         <Swiper
             slidesPerView={2}
             spaceBetween={30}
@@ -84,6 +106,53 @@ const WebProjects = () => {
                 ))
             }
         </Swiper>
+        : 
+        // SWIPER VUE MOBILE TABLETTE
+        <Swiper
+        pagination={{
+            dynamicBullets: true,
+        }}
+        modules={[Pagination]}
+        className="mobileSwiper"
+        >
+            {
+                projetsWeb.map((projet, index)=>(
+                    <SwiperSlide key={index}>
+                        <article className='webproject-card'>
+                            <figure className='webproject-card-bg'>
+                                <img src={projet.cover ? projet.cover : './images/internet.png'} alt={projet.titre} />
+                            </figure>
+                            <div className='webproject-card-content'>
+                                <header>
+                                    <h4>{projet.titre}</h4>
+                                    <p><TbTargetArrow /> 
+                                        {languageEng ? "Project" : "Projet" } {projet.typeProjet}
+                                    </p>
+                                    <h5>{projet.technosTag.map((tech, i)=>(
+                                        <span key={i}>{tech}</span>
+                                    ))}</h5>
+                                </header>
+                                <footer>
+                                    { projet.url ?  
+                                        <button><FaLaptopCode />
+                                            <a href={projet.url} target='_blank'>
+                                                {languageEng ? "See project" : "Voir le projet"}
+                                            </a>
+                                        </button>
+                                        :''
+                                    }
+                                </footer>
+                            </div>
+                            <div className='webproject-card-content-hover'>
+                                <p>{projet.description}</p>
+                            </div>
+                        </article>
+                    </SwiperSlide>
+                ))
+            }
+        </Swiper>
+        }
+        </>
     );
 };
 
